f28a5fcf4190c5920057eb61de9e71b1a8839aea
Frank Hollywood <github@srvsync.com>
2021-09-02 16:31:18 -0700

    worked on map/index.html

--- utils.js
+++ utils.js
@@ -10,4 +10,13 @@ export function setUserInfo(newUser) {
     localStorage.setItem('USER', stringyUser);
 }
 
-findById 
+export function findById(myArray, id) {
+    // loop through the array
+    for (let item of myArray) {
+        // if this item's id matches the id we were passed as an argument . . .
+        if (item.id === id) {
+          // . . . return that item
+            return item;
+        }
+    }
+}
