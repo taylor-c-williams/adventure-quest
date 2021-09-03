f10e867192aa008338ef44c37c2a1f7039fd0413
DanielRGilles <danryangilles@gmail.com>
2021-09-03 09:54:44 -0700

    fixed link create

--- common/is-dead.js
+++ common/is-dead.js
@@ -1,6 +1,13 @@
 // if dead, return user HP<=0
+let user;
 export function isDead(user){
     return user.hp <= 0;
 }
 //a function
-//export 
+//export
+export function gameOver() {
+    if (isDead(user)) {
+        window.location = '../index.html';
+    }
+
+}
